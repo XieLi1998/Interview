f198ea1370c093c93a64e9696c7a685368a8125f
xieli <2524546110@qq.com>
2020-08-31 02:37:41 +0800

    2020/8/31

--- src/高频/reverseList.java
+++ src/高频/reverseList.java
@@ -13,7 +13,7 @@ public class reverseList {
         }
     }
 
-    //206，反转简单链表
+    //206，反转简单链表，非递归
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null)
             return head;
@@ -29,9 +29,41 @@ public class reverseList {
         return pre;
     }
 
-    //92，反转部分链表
+
+    //92，反转部分链表，labuladong
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+
+    //后驱节点
+    ListNode successor = null;
+
+    private ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            // 记录第 n + 1 个节点
+            successor = head.next;
+            return head;
+        }
+
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+
+    //递归反转整个链表
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
     }
 
 }
